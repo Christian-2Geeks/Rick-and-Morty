326b758209f742548c52b8190b513eabc89d1579
Christian Fernandez <christian_alex94@hotmail.com>
2020-04-10 20:53:12 -0400

    agrenago las utilidades para optener el hash y una que pueda mandar la seccion

--- src/pages/Character.js
+++ src/pages/Character.js
@@ -1,11 +1,11 @@
 const Character = () => {
   const view = `
-    <div class="character-inner">
-      <article class="character-card">
+    <div class="Characters-inner">
+      <article class="Characters-card">
         <img src="image" alt="name">
         <h2>Name</h2>
       </article>
-      <article class="character-card">
+      <article class="Characters-card">
         <h3>Episodes:</h3>
         <h3>Status:</h3>
         <h3>Species:</h3>
